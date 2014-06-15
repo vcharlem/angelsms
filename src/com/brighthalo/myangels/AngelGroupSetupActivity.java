f2d705a93048b478be9115eba37ee3d9f9e0d62e
Vladimir <vcharlem@gmail.com>
2014-06-14 22:24:26 -0400

    Select Angels Activity

--- src/com/brighthalo/myangels/AngelGroupSetupActivity.java
+++ src/com/brighthalo/myangels/AngelGroupSetupActivity.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.ContentProviderOperation;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.CursorLoader;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.Menu;
@@ -17,91 +19,110 @@ import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.brighthalo.myangels.MyAngelConstants;
 
 public class AngelGroupSetupActivity extends Activity {
 	public final static String CONTACTNAMESPACE = "ContactsContract.CommonDataKinds";
+	Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+	Uri queryUri = ContactsContract.Contacts.CONTENT_URI;
 	public ArrayList<ContactsContract> listContacts = new ArrayList<ContactsContract>();
-	ListView lvContacts;
-	Cursor 	 cursor1;
+	
+	String selection = ContactsContract.Contacts.DISPLAY_NAME + " IS NOT NULL";
+	String[] projection = new String[] { ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME};		
+	String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
+	ListView lvContacts, lvAngels;
+	public Cursor cursor,	 cursor1, cursorAngels;
+	public ArrayAdapter<String> angelAdapter;
+	public TextView Angel1, Angel2, Angel3, Angel4;
 	ImageButton btnclk;
+	public ArrayList<String> listAngels = new ArrayList<String>();
+	public ArrayList<ContactsContract> listAngelsContract = new ArrayList<ContactsContract>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.angelgroup_setup_screen);
-	//  btnclk = (ImageButton) findViewById(R.id.btnAction);
-	  //btnclk.setFocusable(true);
-		lvContacts = (ListView) findViewById(R.id.listContacts);
-
-		//CursorLoader cursorLoader = new CursorLoader( this,  queryUri,  projection,  selection, null,  null);
-
-		cursor1 = getContentResolver().query(
-				ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-				null, 													// No filter, return all columns
-				null, //ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER + " = 1" , 	// selection,
-				null, 													// selectionArgs,
-				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC"   		// Sorting Order Order
-			);
-		startManagingCursor(cursor1);
-		
 		String [] from_colmn = {
 				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, 
 				ContactsContract.CommonDataKinds.Phone._ID
 		};
+		lvContacts = (ListView) findViewById(R.id.list);
+		Angel1	   = (TextView) findViewById(R.id.nameAngel1);
+		lvAngels   = (ListView) findViewById(R.id.listAngels);
+
+		CursorLoader cursorLoader = new CursorLoader( this,  uri,  projection,  selection, null,  null);
+		cursor = cursorLoader.loadInBackground();
+		cursor1 = getContentResolver().query( uri, from_colmn, null, null,	null );
+		startManagingCursor(cursor1);
 
 		int [] to_item_view = { R.id.contactName};
-	
-		SimpleCursorAdapter listAdapter = 
-			new SimpleCursorAdapter(this, R.layout.mycontact, cursor1, from_colmn, to_item_view, 0);
 		
-    	lvContacts.setClickable(false);
-    	lvContacts.setFocusableInTouchMode(false);
-    	lvContacts.setFocusable(false);
-		lvContacts.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-    	lvContacts.setCacheColorHint(00000000);
-        lvContacts.setSelector(R.drawable.greycellselected);
-		
-		lvContacts.setItemsCanFocus(false);		
-        lvContacts.setAdapter(listAdapter);
-        
+		angelAdapter = new ArrayAdapter<String>(this, R.layout.mycontactangel, R.id.angeltext, listAngels);
+        lvAngels.setAdapter(angelAdapter);
+
+		SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this, R.layout.mycontact, cursor1, from_colmn, to_item_view, 0);
+	     ListAdapter adapter = new SimpleCursorAdapter(
+	                this, R.layout.mycontact, 
+	                cursor, from, to_item_view,  CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+	    lvContacts.setAdapter(adapter);        
 		lvContacts.setOnItemClickListener(new OnItemClickListener() {
 			
 			  @Override
 			  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				cursor.moveToPosition(position);
+				int indexName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+
 				Log.d("myAngels---", " ******  Debug Triggered");
 			    Toast.makeText(AngelGroupSetupActivity.this,
 			      "Click-Click ListItem Number " + position, Toast.LENGTH_LONG).show();
 			  }
-			  
-			/*  @Override
-			  public void onListItemClick(ListView l, View view, int position, long id){
-				  super.onListItemClick(l,view,position,id);
-				  Toast.makeText(AngelGroupSetupActivity.this,
-					      "Click-Click ListItem Number " + pos, Toast.LENGTH_LONG).show()
-			  }*/
 			});	
-		/*			
-		//http://android-er.blogspot.com/2012/11/query-contacts-database-display-in.html
-	/*	Uri queryUri = ContactsContract.Contacts.CONTENT_URI;
-		
-		String[] projection = new String[] { ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME};		
-		String selection = ContactsContract.Contacts.DISPLAY_NAME + " IS NOT NULL";
-		Cursor cursor = cursorLoader.loadInBackground();
-		String[] from = {ContactsContract.Contacts.DISPLAY_NAME};
-		
-	     ListAdapter adapter = new SimpleCursorAdapter(
-	                this, android.R.layout.simple_list_item_1, 
-	                cursor, from, to,  CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-	  */
+		/*	//http://android-er.blogspot.com/2012/11/query-contacts-database-display-in.html */
 	     }
-	
 	public void onActionButtonClick(View view) {
-	    Toast.makeText(AngelGroupSetupActivity.this,
-			      "Click ListItem Number " + lvContacts.getPositionForView(view), 
-			      Toast.LENGTH_LONG).show();
+		
+		cursor.moveToPosition(lvContacts.getPositionForView(view));
+		
+		int indexName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+		int indexNumber = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+		String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+		if ((listAngels.size() < MyAngelConstants.MaxAngelGroup) && !(listAngels.contains(displayName)))  {
+			listAngels.add(displayName);
+		}
+		//listAngelsContract.add(ContactsContract.Data.);
+		
+		Angel1.setText("");
+		
+		//Toast.makeText(AngelGroupSetupActivity.this,
+		//	      "Click ListIte NAME  " + displayName , //lvContacts.getPositionForView(view), 
+		//	      Toast.LENGTH_SHORT).show();
+	}
+	
+	public void setListViewAttributes(){
+		lvContacts.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    	lvContacts.setCacheColorHint(00000000);
+        lvContacts.setSelector(R.drawable.greycellselected);
+
+		lvContacts.setItemsCanFocus(true);
+    	lvContacts.setClickable(true);
+    	lvContacts.setFocusable(false);
+    	lvContacts.setFocusableInTouchMode(false);
+    	
+    	lvAngels.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    	lvAngels.setCacheColorHint(00000000);
+    	lvAngels.setSelector(R.drawable.greycellselected);
+
+    	lvAngels.setItemsCanFocus(true);
+    	lvAngels.setClickable(true);
+    	lvAngels.setFocusable(false);
+    	lvAngels.setFocusableInTouchMode(false);
 	}
 }
---
+++ src/com/brighthalo/myangels/MyAngelConstants.java
@@ -0,0 +1,6 @@
+package com.brighthalo.myangels;
+
+public final class MyAngelConstants {
+	public final static int MaxAngelGroup = 5;
+	public final static int MinAngelGroup = 1;
+}
